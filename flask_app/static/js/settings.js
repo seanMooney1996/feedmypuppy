f55941c60e9f609488dfd0153745641568ccaa99
seanMooney1996 <seanmooney1996@gmail.com>
2025-01-07 19:03:24 +0000

    Added client side to allow for changing of automatic settings Added a calculator to the automatic mode to calculate feed table based on start time, end time, dispense frequency and dispense amount. This will change the settings in the the db for automatic and generate a table for the users to see the exact times the dogs will be fed. Added appropriate mongodb functions to handle changing automatic settings

--- flask_app/static/js/settings.js
+++ flask_app/static/js/settings.js
@@ -38,11 +38,22 @@ function loadSettingsTable(settings) {
     settings.manual_settings.forEach((element, index) => {
       addSettingsTableRow(element,index)
     });
+    settings.automatic_settings.feedTimes.forEach((element) => {
+      addAutomaticSettingsTableRow(element)
+    });
+    const startTimeInput = document.querySelector('#startTime');
+    const endTimeInput = document.querySelector('#endTime');
+    const  amountInput = document.querySelector('#amountInputManual');
+    const  freqInput = document.querySelector('#frequency');
+    startTimeInput.value = settings.automatic_settings.startTime
+    endTimeInput.value = settings.automatic_settings.endTime
+    amountInput.value =  parseInt(settings.automatic_settings.amount,10);
+    freqInput.value = parseInt(settings.automatic_settings.dispenseFreqVal, 10);
 }
 
 
 function addSettingsTableRow(element,index){
-  const settingsBody = document.querySelector('#settingsTableBody');
+  const settingsBody = document.querySelector('#settingsTableBodyManual');
   const row = `
   <tr>
     <td>${element.hour}</td>
@@ -53,15 +64,25 @@ function addSettingsTableRow(element,index){
   settingsBody.insertAdjacentHTML('beforeend', row);
 }
 
+function addAutomaticSettingsTableRow(element){
+  const settingsBody = document.querySelector('#settingsTableBodyAutomatic');
+  const row = `
+  <tr>
+    <td>${element.time}</td>
+    <td>${element.amount}</td>
+  </tr>
+  `;
+  settingsBody.insertAdjacentHTML('beforeend', row);
+}
+
+
 async function addManualSetting(){
     const timeInputField = document.querySelector('#timeInput');
     const amountInputField = document.querySelector('#amountInput');
     if (timeInput.value <0 && timeInput.value>24){
-
         return
     }
     if (amountInputField.value <0.5 && amountInputField.value>10){
-        
         return
     }
     amount = amountInputField.value
@@ -75,7 +96,7 @@ async function addManualSetting(){
           body: JSON.stringify({amount,time})
         });
         if (response.ok) {
-          const settingsBody = document.querySelector('#settingsTableBody');
+          const settingsBody = document.querySelector('#settingsTableBodyManual');
           const lastRow = settingsBody.lastElementChild;
           const newIndex = settingsBody.rows ? settingsBody.rows.length : settingsBody.children.length;
           console.log("Last row:", lastRow);
@@ -93,6 +114,74 @@ async function addManualSetting(){
         console.log("Error in add setting ,", error)
       }
 }
+
+async function addAutomaticSetting(){
+  const startInput = document.querySelector('#startTime');
+  const endInput = document.querySelector('#endTime');
+  const amountInputField = document.querySelector('#amountInputManual');
+  const dispenseFreq = document.querySelector('#frequency');
+  const startTimeComp = new Date(`1970-01-01T${startInput.value}:00`);
+  const endTimeComp = new Date(`1970-01-01T${endInput.value}:00`);
+  if (!startInput.value || !endInput.value) {
+    alert("Start Time and End Time are required!");
+    return;
+}
+  if (startTimeComp >= endTimeComp) {
+      return;
+  }
+  if (!amountInputField.value || amountInputField.value < 0.5 || amountInputField.value > 10) {
+      return;
+  }
+  if (!dispenseFreq.value || isNaN(dispenseFreq.value) || dispenseFreq.value <= 0) {
+      return;
+  }
+  const amount = amountInputField.value
+  const startTime = startInput.value
+  const endTime = endInput.value
+  const dispenseFreqVal = dispenseFreq.value
+  const feedTimes = generateTabledData(amount,startTime,endTime,dispenseFreqVal)
+  try {
+      const response = await fetch('/set_automatic_setting', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({amount,startTime,endTime,dispenseFreqVal,feedTimes})
+      });
+      if (response.ok) {
+        const settingsBody = document.querySelector('#settingsTableBodyAutomatic');
+        settingsBody.innerHTML = ""
+        feedTimes.forEach(row => {
+          addAutomaticSettingsTableRow(row)
+        })
+        
+          console.log("Successful add")
+      } else {
+          console.log("Fail add setting")
+      }
+    } catch (error) {
+      console.log("Error in add setting ,", error)
+    }
+}
+
+function generateTabledData(amount,start,end,freq){
+  const startTime = new Date(`1970-01-01T${start}:00`);
+  const endTime = new Date(`1970-01-01T${end}:00`);
+  const durationMinutes = (endTime - startTime) / (1000 * 60);
+  const amountPerInterval = amount / freq;
+  const feedTimes = [];
+  const intervalMinutes = durationMinutes / (freq - 1); 
+  for (let i = 0; i < freq; i++) {
+    const feedTime = new Date(startTime.getTime() + i* intervalMinutes * 60 * 1000);
+    feedTimes.push({
+      time: feedTime.toTimeString().substring(0, 5),
+      amount: parseFloat(amountPerInterval.toFixed(2)),
+    });
+  }
+  console.log("Feed times:", feedTimes);
+  return feedTimes;
+}
+
 
 async function deleteRow(button,index) {
     try {
